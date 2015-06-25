906ea4fdc8b709e971dbe32a3117053dc678e3f1
Peter Conrad <peter.conrad@swisscollab.ch>
2015-06-25 12:15:46 +0200

    Change identifier string to id_t
    Change the identifier type from string to id_t, which is an alias to
    string, in case we want to change it later.

--- src/expression.d
+++ src/expression.d
@@ -1,7 +1,10 @@
-// An expression, consisting of an identifier string and a variable number
+// data type used as an expression identifier
+alias id_t = string;
+
+// An expression, consisting of an identifier id_t and a variable number
 // of sub-expressions
 struct Expression {
-	string identifier;
+	id_t identifier;
 	Expression[] children;
 }
 
