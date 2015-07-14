861996d21f69bf255f27e7cabf5d4e0d755a66b2
Peter Conrad <peter.conrad@swisscollab.ch>
2015-07-14 15:03:39 +0200

    Fix division by linear functions
    Fix a mistake where division by linear functions would again return
    "linear" as a result. This occured because arithmeticRule simply
    returned the complement of a Property, which for a linear function is
    linear. Make arithmeticRule check for this case and adjust the result
    accordingly. Implement some more functions to check whether a Property
    is linear, increasing, or concave. Add some unit tests to verify the
    functionality of arithmeticRule.

--- src/property.d
+++ src/property.d
@@ -7,6 +7,9 @@ import std.typecons : Tuple;
 alias Property = Tuple!(Curvature, "curv", Gradient, "grad");
 
 // Check for some properties of the Gradient or Curvature
+bool isIncreasing(Gradient g) { return g == Gradient.increasing; }
+bool isDecreasing(Gradient g) { return g == Gradient.decreasing; }
+
 bool isNonDecreasing(Gradient g) {
 	return (g == Gradient.increasing || g == Gradient.constant);
 }
@@ -23,11 +26,16 @@ bool isConcave(Curvature c) {
 	return (c == Curvature.concave || c == Curvature.linear);
 }
 
+bool isLinear(Curvature c) { return c == Curvature.linear; }
+
 // wrapper functions
+bool isIncreasing(Property p) { return isIncreasing(p.grad); }
+bool isDecreasing(Property p) { return isDecreasing(p.grad); }
 bool isNonDecreasing(Property p) { return isNonDecreasing(p.grad); }
 bool isNonIncreasing(Property p) { return isNonIncreasing(p.grad); }
 bool isConvex(Property p) { return isConvex(p.curv); }
 bool isConcave(Property p) { return isConcave(p.curv); }
+bool isLinear(Property p) { return isLinear(p.curv); }
 
 // Property complements, for easy lookup
 enum Curvature[Curvature] cComplement = [
@@ -48,3 +56,7 @@ enum Gradient[Gradient] gComplement = [
 Property complement(Property p) {
 	return Property(cComplement[p.curv], gComplement[p.grad]);
 }
+
+// Returns the single complement of either a Curvature or a Gradient
+Curvature complement(Curvature c) { return cComplement[c]; }
+Gradient complement(Gradient g) { return gComplement[g]; }
--- src/ruleset.d
+++ src/ruleset.d
@@ -44,7 +44,10 @@ Property analyze(Expression e) {
 unittest {
 	assert (analyze(E("2", [])) == P(linear, constant));
 	assert (analyze(E("x", [])) == P(linear, increasing));
-	assert (analyze(E("-", [E("ln", [E("x", [])])])) == P(convex, unspecified));
+	// should be (convex, decreasing) but turns out unknown because of insufficient ruleset for now
+	assert (analyze(E("ln", [E("/", [E("1", []), E("x", [])])])) == P(unknown, unspecified));
+	// should be convex, however by now there is no rule to cover that case
+	assert (analyze(E("*", [E("x", []), E("ln", [E("x", [])])])) == P(unknown, unspecified));
 }
 
 // a rule dealing with simple arithmetic operations
@@ -68,44 +71,84 @@ Property arithmeticRule(Expression e) {
 	if (!(isNumber(e.left) || isNumber(e.right)))
 		return Property(Curvature.unspecified, Gradient.unspecified);
 
-	// Find the scalar and the other expression
+	// Find the scalar and analyze the expression; save the result for later use
 	double number = isNumber(e.left) ? getNumericValue(e.left) : getNumericValue(e.right);
-	Expression other = isNumber(e.left) ? e.right : e.left;
+	Property result = analyze(isNumber(e.left) ? e.right : e.left);
 
 	assert (number != 0); // should be incorrect syntax
 
 	// Addition with a scalar always preserves the properties of the function they are applied to
-	if (e.id == "+") return analyze(other);
+	if (e.id == "+") return result;
 
 	// Subtraction with a scalar preserves the properties if the scalar is on the right side,
 	// otherwise reverse them
 	if (e.id == "-") {
-		if (isNumber(e.right)) return analyze(other);
-		return analyze(other).complement;
+		if (isNumber(e.right)) return result;
+		return result.complement;
 	}
 
 	// Multiplication with a scalar preserves the properties if the scalar is larger than 0 and
 	// reverse them if it is smaller
 	if (e.id == "*") {
-		if (number > 0) return analyze(other);
-		return analyze(other).complement;
+		if (number > 0) return result;
+		return result.complement;
 	}
 
 	// Division with a scalar depends on both the side of the scalar and whether it is smaller or
-	// larger than 0
+	// larger than 0; also special rules apply if the function divided by has linear property
 	if (e.id == "/") {
-		if (isNumber(e.left) && number > 0) return analyze(other).complement;
-		if (isNumber(e.left) && number < 0) return analyze(other);
-		if (isNumber(e.right) && number > 0) return analyze(other);
-		if (isNumber(e.right) && number < 0) return analyze(other).complement;
+		if (isNumber(e.left)) {
+			// First check whether the function divided is linear. The curvature and gradient need
+			// to be adjusted accordingly
+			if (number > 0 && result.isLinear) {
+				if (result.isIncreasing) return Property(Curvature.convex, Gradient.decreasing);
+				return Property(Curvature.concave, Gradient.increasing);
+			}
+			if (number < 0 && result.isLinear) {
+				if (result.isIncreasing) return Property(Curvature.concave, Gradient.increasing);
+				return Property(Curvature.convex, Gradient.decreasing);
+			}
+			// for non-linear functions the result only depends on whether the number is smaller or
+			// larger than zero
+			if (number > 0) return result.complement;
+			if (number < 0) return result;
+		}
+		if (isNumber(e.right) && number > 0) return result;
+		if (isNumber(e.right) && number < 0) return result.complement;
 	}
 
 	return Property(Curvature.unspecified, Gradient.unspecified);
 }
 
 unittest {
+	// two numbers
 	assert (arithmeticRule(E("+", [E("2.5", []), E("-2", [])])) == P(linear, constant));
+
+	// unary minus and plus
+	assert (arithmeticRule(E("-", [E("ln", [E("x", [])])])) == P(convex, unspecified));
+	assert (arithmeticRule(E("+", [E("ln", [E("x", [])])])) == P(concave, unspecified));
+
+	// addition, subtraction, multiplication
+	assert (arithmeticRule(E("+", [E("ln", [E("x", [])]), E("2", [])])) == P(concave, unspecified));
+	assert (arithmeticRule(E("-", [E("ln", [E("x", [])]), E("2", [])])) == P(concave, unspecified));
+	assert (arithmeticRule(E("-", [E("ln", [E("x", [])]), E("-2", [])])) == P(concave, unspecified));
+	assert (arithmeticRule(E("-", [E("2", []), E("ln", [E("x", [])])])) == P(convex, unspecified));
+	assert (arithmeticRule(E("*", [E("ln", [E("x", [])]), E("2", [])])) == P(concave, unspecified));
 	assert (arithmeticRule(E("*", [E("ln", [E("x", [])]), E("-2", [])])) == P(convex, unspecified));
+	assert (arithmeticRule(E("/", [E("1", []), E("ln", [E("x", [])])])) == P(convex, unspecified));
+	assert (arithmeticRule(E("/", [E("1", []), E("-", [E("ln", [E("x", [])])])])) == P(concave, unspecified));
+
+	// division by linear functions, using some more complex examples
+	assert (arithmeticRule(E("/", [E("1", []), E("x", [])])) == P(convex, decreasing));
+	assert (arithmeticRule(E("/", [E("1", []), E("-", [E("x", [])])])) == P(concave, increasing));
+	assert (arithmeticRule(E("/", [E("1", []), E("+", [E("*", [E("-2", []), E("x", [])]), E("5", [])])]))
+				== P(concave, increasing));
+	assert (arithmeticRule(E("/", [E("1", []), E("-", [E("+", [E("*", [E("-2", []), E("x", [])]),
+							E("5", [])])])])) == P(convex, decreasing));
+	assert (arithmeticRule(E("/", [E("-1", []), E("+", [E("*", [E("-2", []), E("x", [])]),
+							E("5", [])])])) == P(convex, decreasing));
+	assert (arithmeticRule(E("/", [E("-1", []), E("-", [E("+", [E("*", [E("-2", []), E("x", [])]),
+							E("5", [])])])])) == P(concave, increasing));
 }
 
 // properties of already known functions, to be used with the composition rule
